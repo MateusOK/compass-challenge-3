07a8e1553b5fe8d70dabf4fda11f8d7d151b88bd
MateusOK <mateusribeiro-2004@hotmail.com>
2023-08-12 10:28:18 -0300

    feat: initial commit

---
+++ src/main/java/br/com/pb/compass/challange3/Challange3Application.java
@@ -0,0 +1,13 @@
+package br.com.pb.compass.challange3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Challange3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Challange3Application.class, args);
+	}
+
+}
