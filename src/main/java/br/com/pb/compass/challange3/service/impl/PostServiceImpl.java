41e8c8a873818458d31cbd5cd0e5239743b86441
MateusOK <mateusribeiro-2004@hotmail.com>
2023-08-13 15:48:42 -0300

    refactor: use lambda expressions and throw custom exceptions

--- src/main/java/br/com/pb/compass/challange3/service/impl/PostServiceImpl.java
+++ src/main/java/br/com/pb/compass/challange3/service/impl/PostServiceImpl.java
@@ -3,6 +3,7 @@ package br.com.pb.compass.challange3.service.impl;
 import br.com.pb.compass.challange3.dto.PostResponseDto;
 import br.com.pb.compass.challange3.entity.History;
 import br.com.pb.compass.challange3.entity.Post;
+import br.com.pb.compass.challange3.exception.ResourceNotFoundException;
 import br.com.pb.compass.challange3.repository.PostRepository;
 import br.com.pb.compass.challange3.service.PostService;
 import br.com.pb.compass.challange3.utils.HistoryStatus;
@@ -13,7 +14,6 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -36,7 +36,7 @@ public class PostServiceImpl implements PostService {
         var postsDto = new ArrayList<PostResponseDto>();
         posts.forEach(post -> postsDto.add(new PostResponseDto(post)));
         if (posts.isEmpty()){
-            throw new RuntimeException("No posts were found");
+            throw new ResourceNotFoundException("No posts were found");
         }
         return postsDto;
     }
@@ -51,53 +51,42 @@ public class PostServiceImpl implements PostService {
 
     @Override
     public void enablePost(Long postId){
-        Optional<Post> postOptional = postRepository.findById(postId);
-        if (postOptional.isPresent()){
-            Post post = postOptional.get();
-            post.getHistory().add(new History(LocalDateTime.now(), HistoryStatus.ENABLED.name(), post));
-            postRepository.save(post);
-        }
+        Post post = postRepository.findById(postId)
+            .orElseThrow(() -> new ResourceNotFoundException("Post not found with id: " + postId));
+        post.getHistory().add(new History(LocalDateTime.now(), HistoryStatus.ENABLED.name(), post));
+        postRepository.save(post);
     }
 
     @Override
     public void disablePost(Long postId) {
-        Optional<Post> postOptional = postRepository.findById(postId);
+        Post post = postRepository.findById(postId)
+            .orElseThrow(() -> new ResourceNotFoundException("Post not found with id: " + postId));
 
-        if (postOptional.isPresent()) {
-            Post post = postOptional.get();
+        List<History> historyList = post.getHistory();
+        if (historyList.isEmpty()) {
+            throw new IllegalStateException("Post history is empty.");
+        }
 
-            List<History> historyList = post.getHistory();
-            if (historyList.isEmpty()) {
-                throw new IllegalStateException("Post history is empty.");
-            }
-
-            History latestHistory = historyList.get(historyList.size() - 1);
-            if (latestHistory.getStatus().equals(HistoryStatus.ENABLED.name())) {
-                post.getHistory().add(new History(LocalDateTime.now(), HistoryStatus.DISABLED.name(), post));
-                postRepository.save(post);
-            } else if (latestHistory.getStatus().equals(HistoryStatus.DISABLED.name())) {
-                throw new RuntimeException("Post is already disabled");
-            } else {
-                throw new IllegalStateException("Latest history is not in the ENABLED state.");
-            }
+        History latestHistory = historyList.get(historyList.size() - 1);
+        if (latestHistory.getStatus().equals(HistoryStatus.ENABLED.name())) {
+            post.getHistory().add(new History(LocalDateTime.now(), HistoryStatus.DISABLED.name(), post));
+            postRepository.save(post);
+        } else if (latestHistory.getStatus().equals(HistoryStatus.DISABLED.name())) {
+            throw new IllegalStateException("Post is already disabled");
         } else {
-            throw new RuntimeException("Post not found.");
+            throw new IllegalStateException("Latest history is not in the ENABLED state.");
         }
     }
 
     @Override
     public void updatePost(Long postId) {
-        Optional<Post> postOptional = postRepository.findById(postId);
+        Post post = postRepository.findById(postId)
+            .orElseThrow(() -> new ResourceNotFoundException("Post not found with id: " + postId));
+        if (isPostInEnabledOrDisabledState(post)) {
+            post.getHistory().add(new History(LocalDateTime.now(), HistoryStatus.UPDATING.name(), post));
 
-        if (postOptional.isPresent()) {
-            Post post = postOptional.get();
-
-            if (isPostInEnabledOrDisabledState(post)) {
-                post.getHistory().add(new History(LocalDateTime.now(), HistoryStatus.UPDATING.name(), post));
-
-                postRepository.save(post);
-                jmsTemplate.convertAndSend("post.find.queue", postId);
-            }
+            postRepository.save(post);
+            jmsTemplate.convertAndSend("post.find.queue", postId);
         }
     }
 
