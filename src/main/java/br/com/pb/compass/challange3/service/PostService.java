2a8c54d4e29ac3e87eef33f11280a13580e4d683
MateusOK <mateusribeiro-2004@hotmail.com>
2023-08-13 13:33:08 -0300

    feat: create new post method

--- src/main/java/br/com/pb/compass/challange3/service/PostService.java
+++ src/main/java/br/com/pb/compass/challange3/service/PostService.java
@@ -1,4 +1,7 @@
 package br.com.pb.compass.challange3.service;
 
 public interface PostService {
+
+    void createPost(Long id);
+
 }
--- src/main/java/br/com/pb/compass/challange3/service/impl/PostServiceImpl.java
+++ src/main/java/br/com/pb/compass/challange3/service/impl/PostServiceImpl.java
@@ -1,8 +1,26 @@
 package br.com.pb.compass.challange3.service.impl;
 
+import br.com.pb.compass.challange3.entity.History;
+import br.com.pb.compass.challange3.entity.Post;
+import br.com.pb.compass.challange3.repository.PostRepository;
 import br.com.pb.compass.challange3.service.PostService;
+import br.com.pb.compass.challange3.utils.HistoryStatus;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+
 @Service
+@RequiredArgsConstructor
 public class PostServiceImpl implements PostService {
+
+    private final PostRepository postRepository;
+
+    @Override
+    public void createPost(Long postId) {
+        Post post = new Post();
+        post.setId(postId);
+        post.getHistory().add(new History(LocalDateTime.now(), HistoryStatus.CREATED.name(), post));
+        postRepository.save(post);
+    }
 }
