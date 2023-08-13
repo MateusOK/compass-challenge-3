4fab9f8dc5159ade46b5b9f3915b090a044ca5b7
MateusOK <mateusribeiro-2004@hotmail.com>
2023-08-13 14:49:58 -0300

    feat: comments ok queue

---
+++ src/main/java/br/com/pb/compass/challange3/message/CommentsOkQueue.java
@@ -0,0 +1,51 @@
+package br.com.pb.compass.challange3.message;
+
+
+import br.com.pb.compass.challange3.entity.Comment;
+import br.com.pb.compass.challange3.entity.History;
+import br.com.pb.compass.challange3.entity.Post;
+import br.com.pb.compass.challange3.repository.PostRepository;
+import br.com.pb.compass.challange3.service.ExternalApi;
+import br.com.pb.compass.challange3.utils.HistoryStatus;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+
+import java.time.LocalDateTime;
+
+@Component
+@RequiredArgsConstructor
+public class CommentsOkQueue {
+
+    private final JmsTemplate jmsTemplate;
+    private final PostRepository postRepository;
+    private final ExternalApi externalApi;
+
+    @JmsListener(destination = "comments.ok.queue")
+    public void processOkComments(Long postId) {
+
+        Post post = postRepository.findById(postId)
+            .orElseThrow(() -> new RuntimeException("Post not found" + postId));
+        if (post.getComments().size() == 5){
+            post.getHistory().add(new History(LocalDateTime.now(), HistoryStatus.COMMENTS_OK.name(), post));
+            postRepository.save(post);
+            jmsTemplate.convertAndSend("enabled.queue", post.getId());
+        }
+        Flux<Comment> commentFlux = externalApi.fetchCommentForPost(postId);
+
+        commentFlux.collectList().subscribe(
+                commentList -> {
+                    commentList.forEach(comment -> comment.setPostId(postId));
+                    post.getComments().addAll(commentList);
+
+                    post.getHistory().add(new History(LocalDateTime.now(), HistoryStatus.COMMENTS_OK.name(), post));
+
+                    postRepository.save(post);
+
+                    jmsTemplate.convertAndSend("enabled.queue", post.getId());
+                }
+        );
+    }
+}
