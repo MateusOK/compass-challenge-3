e1a35804b94f1a683675250843024dd512a4ccfe
MateusOK <mateusribeiro-2004@hotmail.com>
2023-08-13 15:39:25 -0300

    feat: spring validation annotations to avoid bad requests

--- src/main/java/br/com/pb/compass/challange3/controller/PostController.java
+++ src/main/java/br/com/pb/compass/challange3/controller/PostController.java
@@ -2,9 +2,13 @@ package br.com.pb.compass.challange3.controller;
 
 import br.com.pb.compass.challange3.dto.PostResponseDto;
 import br.com.pb.compass.challange3.service.PostService;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,13 +22,14 @@ import java.util.List;
 @RestController
 @RequestMapping("/posts")
 @RequiredArgsConstructor
+@Validated
 public class PostController {
 
     private final JmsTemplate jmsTemplate;
     private final PostService postService;
 
     @PostMapping("/{postId}")
-    public ResponseEntity<Void> processPost(@PathVariable Long postId){
+    public ResponseEntity<Void> processPost(@PathVariable @Positive @Max(100) @Min(1) Long postId){
         jmsTemplate.convertAndSend("created.queue", postId);
         return ResponseEntity.accepted().build();
     }
@@ -36,13 +41,13 @@ public class PostController {
     }
 
     @DeleteMapping("/{postId}")
-    public ResponseEntity<Void> disablePost(@PathVariable Long postId){
+    public ResponseEntity<Void> disablePost(@PathVariable @Positive @Max(100) @Min(1) Long postId){
         jmsTemplate.convertAndSend("disabled.queue", postId);
         return ResponseEntity.noContent().build();
     }
 
     @PutMapping("/{postId}")
-    public ResponseEntity<Void> updatePost(@PathVariable Long postId){
+    public ResponseEntity<Void> updatePost(@PathVariable @Positive @Max(100) @Min(1) Long postId){
         postService.updatePost(postId);
         return ResponseEntity.accepted().build();
     }
