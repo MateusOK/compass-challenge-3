9f93598b24f79d516e95633f4fd3ab068dc82429
MateusOK <mateusribeiro-2004@hotmail.com>
2023-08-14 11:23:33 -0300

    fix: logic throwing illegal state exception when updating

--- src/main/java/br/com/pb/compass/challange3/message/CommentsOkQueue.java
+++ src/main/java/br/com/pb/compass/challange3/message/CommentsOkQueue.java
@@ -1,9 +1,9 @@
 package br.com.pb.compass.challange3.message;
 
-
 import br.com.pb.compass.challange3.entity.Comment;
 import br.com.pb.compass.challange3.entity.History;
 import br.com.pb.compass.challange3.entity.Post;
+import br.com.pb.compass.challange3.exception.ResourceNotFoundException;
 import br.com.pb.compass.challange3.repository.PostRepository;
 import br.com.pb.compass.challange3.service.ExternalApi;
 import br.com.pb.compass.challange3.utils.HistoryStatus;
@@ -27,17 +27,13 @@ public class CommentsOkQueue {
     public void processOkComments(Long postId) {
 
         Post post = postRepository.findById(postId)
-            .orElseThrow(() -> new RuntimeException("Post not found" + postId));
-        if (post.getComments().size() == 5){
-            post.getHistory().add(new History(LocalDateTime.now(), HistoryStatus.COMMENTS_OK.name(), post));
-            postRepository.save(post);
-            jmsTemplate.convertAndSend("enabled.queue", post.getId());
-        }
+            .orElseThrow(() -> new ResourceNotFoundException("Post not found" + postId));
         Flux<Comment> commentFlux = externalApi.fetchCommentForPost(postId);
 
         commentFlux.collectList().subscribe(
                 commentList -> {
                     commentList.forEach(comment -> comment.setPostId(postId));
+                    post.getComments().clear();
                     post.getComments().addAll(commentList);
 
                     post.getHistory().add(new History(LocalDateTime.now(), HistoryStatus.COMMENTS_OK.name(), post));
